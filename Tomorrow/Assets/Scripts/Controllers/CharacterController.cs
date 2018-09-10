62fd67557918e7c24fef3a486f6dcd467511879e
Robin Ostner <Ostner_Robin@web.de>
2018-09-10 22:33:15 +0200

    Added Running

---
+++ Tomorrow/Assets/Scripts/Controllers/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    private Rigidbody2D characterRigidbody;
+
+    private float horizontalInput;
+    
+    private float currentSpeed;
+    public float horizontalSpeed;
+    
+	void Start () {
+        characterRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        HandleInputs();
+
+        GetMotionData();
+
+        ApplyMotion();
+	}
+
+    private void HandleInputs()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void ApplyMotion()
+    {
+        Vector2 horizontalVelocity = Vector2.right * horizontalInput * horizontalSpeed;
+        characterRigidbody.velocity = horizontalVelocity + Vector2.up * characterRigidbody.velocity.y;
+    }
+
+    private void GetMotionData()
+    {
+        currentSpeed = characterRigidbody.velocity.magnitude;
+    }
+}
