f85a66d5796ddfe0c9edc71d607afbca9a6f8316
Robin Ostner <Ostner_Robin@web.de>
2018-09-20 14:59:11 +0200

    Implemented Shooting

--- Tomorrow/Assets/Scripts/Controllers/CharacterController.cs
+++ Tomorrow/Assets/Scripts/Controllers/CharacterController.cs
@@ -29,10 +29,37 @@ public class CharacterController : MonoBehaviour {
     private int maxExtraJumps;
     private int extraJumps;
 
-    private bool facingRight;
+    private bool facingRight = true;
 
-    
-	void Start () {
+    private Vector3 mousePosition;
+    private Vector3 mousePositionWorld;
+    private Vector3 shootingDirection;
+
+    [SerializeField]
+    private GameObject shoulder;
+    [SerializeField]
+    private GameObject elbow;
+    [SerializeField]
+    private GameObject hand;
+    [SerializeField]
+    private GameObject upperArm;
+    [SerializeField]
+    private GameObject lowerArm;
+
+    private float upperArmAngle;
+    private float lowerArmAngle;
+    private float upperAngleOffset;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+    private bool wantsToShoot;
+    [SerializeField]
+    private float shootingCoolDown;
+    private float shootingCoolDownTimer;
+    [SerializeField]
+    private float bulletSpeed;
+
+    void Start () {
         characterRigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
@@ -44,6 +71,13 @@ public class CharacterController : MonoBehaviour {
 
         HandleInputs();
         HandleJumps();
+
+        CalculateShootingDirection();
+        PositionArms();
+
+        HandleCoolDown();
+
+        HandleShooting();
 
         GetMotionData();
 	}
@@ -57,15 +91,11 @@ public class CharacterController : MonoBehaviour {
     {
         horizontalInput = Input.GetAxis("Horizontal");
         wantsToJump = Input.GetButtonDown("Jump");
+        wantsToShoot = Input.GetButton("Fire1");
 
         float absoluteHorizontal = Mathf.Abs(horizontalInput);
         animator.SetFloat("xAxis", absoluteHorizontal);
         animator.speed = absoluteHorizontal;
-
-        if (!facingRight && horizontalInput > 0 || facingRight && horizontalInput < 0)
-        {
-            FlipCharacter();
-        }
     }
 
     private void ApplyMotion()
@@ -87,6 +117,28 @@ public class CharacterController : MonoBehaviour {
         }
     }
 
+    private void HandleShooting()
+    {
+        if(wantsToShoot && shootingCoolDownTimer == 0)
+        {
+            Shoot();
+            shootingCoolDownTimer = shootingCoolDown;
+        }
+    }
+
+    private void Shoot()
+    {
+        BulletController bullet = Instantiate(bulletPrefab, hand.transform.position, Quaternion.identity).GetComponent<BulletController>();
+        bullet.Initialize(shootingDirection, bulletSpeed);
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), bullet.GetComponent<Collider2D>());
+    }
+
+    private void HandleCoolDown()
+    {
+        shootingCoolDownTimer -= Time.deltaTime;
+        shootingCoolDownTimer = Mathf.Clamp(shootingCoolDownTimer, 0, shootingCoolDown);
+    }
+
     private void CheckGrounded()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, floorLayerMask);
@@ -102,9 +154,48 @@ public class CharacterController : MonoBehaviour {
         currentSpeed = characterRigidbody.velocity.magnitude;
     }
 
-    private void FlipCharacter()
+    public void FlipCharacter()
     {
         facingRight = !facingRight;
         spriteRenderer.flipX = !facingRight;
+        shoulder.transform.position -= Vector3.right*(shoulder.transform.position.x-transform.position.x)*2;
+    }
+
+    private void CalculateShootingDirection()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+        mousePositionWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        shootingDirection = (mousePositionWorld - transform.position).normalized;
+    }
+
+    private void PositionArms()
+    {
+        float shootingDirectionAngle = Vector3.Angle(shootingDirection, Vector3.down);
+        if (shootingDirection.x < 0)
+        {
+            shootingDirectionAngle *= -1;
+        }
+
+        upperAngleOffset = Mathf.Cos((shootingDirectionAngle - 90) / 180 * Mathf.PI) * 80;
+
+        lowerArmAngle = shootingDirectionAngle;
+        upperArmAngle = shootingDirectionAngle - upperAngleOffset;
+
+        Vector3 upperDirection = Quaternion.Euler(0, 0, -upperAngleOffset) * shootingDirection;
+        upperArm.transform.position = shoulder.transform.position + upperArm.transform.localScale.y * upperDirection / 2f;
+        upperArm.transform.rotation = Quaternion.identity;
+        upperArm.transform.Rotate(Vector3.forward, upperArmAngle);
+
+        Vector3 lowerDirection = shootingDirection;
+        lowerArm.transform.position = elbow.transform.position + lowerArm.transform.localScale.y * shootingDirection / 2f;
+        lowerArm.transform.rotation = Quaternion.identity;
+        lowerArm.transform.Rotate(Vector3.forward, lowerArmAngle);
+
+        if (shootingDirection.x < 0 && facingRight || shootingDirection.x >= 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
     }
 }
