2d519f13142eab1af4f160dabd8d272eecc56d1d
Robin Ostner <Ostner_Robin@web.de>
2018-09-25 23:18:45 +0200

    Sounds Working

---
+++ Tomorrow/Assets/Scripts/Managers/CharacterAudioManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAudioManager : MonoBehaviour {
+
+    public float lowestPitch = 0.95f;
+    public float highestPitch = 1.05f;
+
+    [SerializeField]
+    private AudioClip shootingClip;
+    [SerializeField]
+    private AudioClip walkingClip;
+    [SerializeField]
+    private AudioClip runningClip;
+    [SerializeField]
+    private AudioClip jumpingClip;
+    [SerializeField]
+    private AudioClip landingClip;
+
+    private AudioSource source;
+
+    private float walkSoundTimer;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        HandleWalkSoundTimer();
+    }
+
+    private void HandleWalkSoundTimer()
+    {
+        walkSoundTimer -= Time.deltaTime;
+    }
+
+    private void PlayClipOneShot(AudioClip clip)
+    {
+        float randomPitch = Random.Range(lowestPitch, highestPitch);
+        source.pitch = randomPitch;
+        source.PlayOneShot(clip);
+    }
+
+    public void PlayWalkingSound()
+    {
+        if (walkSoundTimer <= 0)
+        {
+            PlayClipOneShot(walkingClip);
+            walkSoundTimer = 0.1f;
+        }
+    }
+
+    public void PlayRunningSound()
+    {
+        PlayClipOneShot(runningClip);
+    }
+
+    public void PlayShootingSound()
+    {
+        PlayClipOneShot(shootingClip);
+    }
+
+    public void PlayJumpSound()
+    {
+        PlayClipOneShot(jumpingClip);
+    }
+
+    public void PlayLandingSound()
+    {
+        PlayClipOneShot(landingClip);
+    }
+}
