4946537775346a8ae54ac7f60ffc7ec86fd3c8b5
Robin Ostner <Ostner_Robin@web.de>
2018-10-01 23:54:12 +0200

    Add Lilith Walk Ability

--- Tomorrow/Assets/Scripts/Controllers/LilithController.cs
+++ Tomorrow/Assets/Scripts/Controllers/LilithController.cs
@@ -8,6 +8,7 @@ struct SurroundingCheck
     public Vector2 direction;
     public float distance;
     public LayerMask layer;
+    public Vector2 normal;
 }
 
 public class LilithController : MonoBehaviour {
@@ -19,21 +20,36 @@ public class LilithController : MonoBehaviour {
     private LayerMask walkableLayerMask;
 
     [SerializeField]
+    private float floorDistance;
+
+    [SerializeField]
     private Transform groundCheckTransform;
     private bool isGrounded;
     [SerializeField]
+    private Transform headCheckTransform;
+    [SerializeField]
+    private bool isOnHead;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
     private float surroundingCheckRadius;
+    [SerializeField]
+    private float minWallDistance;
+    private float distanceToWall;
     
     private SurroundingCheck downRay = new SurroundingCheck();
     private SurroundingCheck frontRay = new SurroundingCheck();
     private SurroundingCheck backRay = new SurroundingCheck();
     private SurroundingCheck upRay = new SurroundingCheck();
+    private SurroundingCheck walkForwardRay = new SurroundingCheck();
+    
+    private Vector2 walkDirection;
+    [SerializeField]
+    private float walkSpeed;
 
     // Use this for initialization
     void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
-
-        rigidbody.isKinematic = true;
 
         SetupSurroundingChecks();
 	}
@@ -41,15 +57,12 @@ public class LilithController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         CheckSurroundings();
-	}
+        CheckGrounded();
 
-    private void SetupSurroundingChecks()
-    {
-        downRay.direction = Vector2.down;
-        frontRay.direction = Vector2.right;
-        backRay.direction = Vector2.left;
-        upRay.direction = Vector2.up;
-    }
+        CalculateWalkDirection();
+
+        HandleMovement();
+	}
 
     private void OnDrawGizmos()
     {
@@ -65,28 +78,105 @@ public class LilithController : MonoBehaviour {
         
         Gizmos.DrawRay(transform.position, upRay.ray.direction * upRay.distance);
         Gizmos.DrawWireSphere(transform.position + (Vector3)upRay.ray.direction * upRay.distance, 0.25f);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(transform.position, (Vector3)walkDirection * 3f);
+        Gizmos.DrawWireSphere(transform.position + (Vector3)walkDirection * 3f, 0.25f);
+    }
+
+    private void SetupSurroundingChecks()
+    {
+        downRay.direction = Vector2.down;
+        frontRay.direction = Vector2.right;
+        backRay.direction = Vector2.left;
+        upRay.direction = Vector2.up;
     }
 
     private void CheckSurroundings()
     {
-        downRay = CheckRay(downRay);
-        frontRay = CheckRay(frontRay);
-        backRay = CheckRay(backRay);
-        upRay = CheckRay(upRay);
+        downRay = CheckRay(downRay, true);
+        frontRay = CheckRay(frontRay, true);
+        backRay = CheckRay(backRay, true);
+        upRay = CheckRay(upRay, true);
     }
 
-    private SurroundingCheck CheckRay(SurroundingCheck surroundingCheck)
+    private SurroundingCheck CheckRay(SurroundingCheck surroundingCheck, bool useLocalRotation)
     {
         surroundingCheck.distance = surroundingCheckRadius;
-        surroundingCheck.ray = new Ray2D(transform.position, transform.rotation * surroundingCheck.direction * surroundingCheckRadius);
+        if (useLocalRotation)
+        {
+            surroundingCheck.ray = new Ray2D(transform.position, transform.rotation * surroundingCheck.direction * surroundingCheckRadius);
+        }
+        else
+        {
+            surroundingCheck.ray = new Ray2D(transform.position, surroundingCheck.direction * surroundingCheckRadius);
+        }
 
         RaycastHit2D result = Physics2D.Raycast(transform.position, surroundingCheck.ray.direction, surroundingCheckRadius, walkableLayerMask);
         if (result.collider != null)
         {
             surroundingCheck.distance = result.distance;
             surroundingCheck.layer = LayerMask.GetMask(LayerMask.LayerToName(result.collider.gameObject.layer));
+            surroundingCheck.normal = result.normal;
         }
 
         return surroundingCheck;
+    }
+
+    private void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, walkableLayerMask);
+
+        rigidbody.isKinematic = isGrounded;
+
+        if (isGrounded)
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+    }
+
+    private void CalculateWalkDirection()
+    {
+        if (isGrounded)
+        {
+            walkDirection = Quaternion.Euler(0,0,-90) * downRay.normal;
+            walkForwardRay.direction = walkDirection;
+            walkForwardRay = CheckRay(walkForwardRay, false);
+        }
+        else
+        {
+            walkDirection = Vector2.zero;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if(isGrounded)
+        {
+            rigidbody.velocity = walkDirection * walkSpeed;
+
+            if(walkForwardRay.distance <= floorDistance)
+            {
+                transform.Rotate(0, 0, 5);
+            }
+            else
+            {
+                transform.rotation = CalculateNormalRotation();
+                AdjustToFloorDistance();
+            }
+        }
+    }
+
+    private Quaternion CalculateNormalRotation()
+    {
+        return Quaternion.LookRotation(Vector3.forward, downRay.normal);
+    }
+    
+    private void AdjustToFloorDistance()
+    {
+        CheckSurroundings();
+
+        Vector3 intersect = transform.position + transform.rotation*downRay.direction * downRay.distance;
+        transform.position = intersect - transform.rotation*downRay.direction.normalized * floorDistance;
     }
 }
